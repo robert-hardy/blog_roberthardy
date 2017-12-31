4dc215b4c20088dc2140e3b8eebf7e152baa6dcb
Robert Hardy <robbert@gmail.com>
2017-12-31 08:27:51 +0000

    Try setting SITEURL to fix the missing css in GitHub.

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Robert Hardy'
 SITENAME = 'Macro Data Research'
-SITEURL = ''
+SITEURL = 'https://robert-hardy.github.io/macrodataresearch'
 
 PATH = 'content'
 
