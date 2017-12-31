2f1587c903586de65a87a1daed866fe07abc11b8
Robert Hardy <robbert@gmail.com>
2017-12-31 08:35:21 +0000

    Typo on SITEURL.

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://robert-hardy.github.io/macrodataresearch'
+SITEURL = 'https://robert-hardy.github.io/macrodataresearchblog'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
