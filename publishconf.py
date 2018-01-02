2dbfa0723d1091f8a2ff10fc4d6d170aace0eeb2
Robert Hardy <robbert@gmail.com>
2018-01-02 06:53:52 +0000

    Update SITEURL since I just changed the repo name.

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://robert-hardy.github.io/macrodataresearchblog'
+SITEURL = 'https://robert-hardy.github.io/blog_roberthardy'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
